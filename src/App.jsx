3e3e5476a3113d9cd4b9963f2c2b0237480570f3
0xNike <104839424+0xNike@users.noreply.github.com>
2022-08-28 17:35:59 +0000

    Updated App

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ function MyTimer({ expiryTimestamp }) {
   } = useTimer({
     expiryTimestamp, autoStart: false, onExpire: () => {
       timesUp();
+      Break();
     }
   });
 
@@ -53,8 +54,15 @@ function MyTimer({ expiryTimestamp }) {
 
   function updateTime() {
     var no = document.querySelector('#duration').value;
-    setTime = Number(no)
-    document.querySelector('#disDuration').innerText = no + " mins"
+    setTime = Number(no);
+    document.querySelector('#disDuration').innerText = no + " mins";
+  }
+
+  function Break() {
+    document.querySelector('#disDuration').innerText = "Time for a break!";
+    document.querySelector('body').style.background = "#A8C4C7";
+    document.querySelector('.pomo').style.background = "#A8C4C7";
+    document.querySelector('.pomo').style.boxShadow = " 20px 20px 60px #8fa7a9, -20px -20px 60px #c1e1e5"
   }
 
   function startPomo() {
@@ -63,6 +71,9 @@ function MyTimer({ expiryTimestamp }) {
     document.querySelector("#start-btn").style.display = "none";
     document.querySelector("#resume-btn").style.display = "initial";
     document.querySelector("#stop-btn").style.display = "initial";
+    document.querySelector('body').style.background = "#A3D5FF";
+    document.querySelector('.pomo').style.background = "#A3D5FF";
+    document.querySelector('.pomo').style.boxShadow = "26px 26px 53px #8bb5d9, -26px -26px 53px #bbf5ff";
   }
 
   function stopPomo() {
@@ -88,7 +99,7 @@ function MyTimer({ expiryTimestamp }) {
       
       {/* Timer Setter*/}
       <div className="timeSetter">
-        <h2 id="disDuration">Set your duration</h2>
+        <h2 id="disDuration">25 mins</h2>
         <input
           id="duration"
           type="range"
